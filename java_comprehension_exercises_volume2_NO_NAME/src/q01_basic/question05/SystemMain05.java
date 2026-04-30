3c24cc2b0847e1847aa5a2ab914b944602203380
shwatana <shwatana@C010423733.zipangu.cac.co.jp>
2026-04-30 12:28:05 +0900

    4/30 q01_basic.question06まで完了しました。

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question05/SystemMain05.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question05/SystemMain05.java
@@ -5,8 +5,21 @@ public class SystemMain05 {
 	public static void main(String[] args) {
 
 		AbstMember[] members = new AbstMember[2];
-		//TODO ここから実装する
 
+		// NonMember
+		members[0] = new NonMember("Sato Kensuke");
+
+		// Member
+		members[1] = new Member(1, "Passw0rd", "Miura Manabu", 28, 2);
+
+		// 表示
+		MemberManager.showAllMembers(members);
+
+		// 購入処理
+		System.out.println("---BUY ITEM---");
+		for (AbstMember m : members) {
+			m.buyItem();
+		}
 	}
 
 }
---
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question06/Member.java
@@ -0,0 +1,81 @@
+package q01_basic.question06;
+
+public class Member extends AbstMember {
+	private int id;
+	private String password;
+	private int age;
+	private int rank;
+
+	// 引数なしコンストラクタ
+	public Member() {
+	}
+
+	// 引数ありコンストラクタ
+	public Member(int id, String password, String name, int age, int rank) {
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.age = age;
+		this.rank = rank;
+	}
+
+	// getter / setter
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	// buyItem メソッド
+	@Override
+	public void buyItem() {
+		System.out.println("商品を購入しました。");
+	}
+
+	// showMember メソッド
+	@Override
+	public void showMember() {
+		System.out.println("***MEMBER DATA***");
+		System.out.println("id:" + id);
+		System.out.println("password:" + password);
+		System.out.println("name:" + name);
+		System.out.println("age:" + age);
+		System.out.println("rank:" + rank);
+		System.out.println("*****************");
+	}
+
+}
